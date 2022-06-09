902b3aaff03c7a5dccc164b87f18de32358cf4ed
kuldeepraw414 <kuldeeprawat.kr@gmail.com>
2022-06-09 20:42:23 +0530

    changes

--- src/components/Collection/Collection.js
+++ src/components/Collection/Collection.js
@@ -1,251 +1,251 @@
-import { React, useState} from 'react';
+import { React, useState } from 'react';
 import ViewMoreBtn from "../ViewMoreBtn/index";
 import "./Collection.css";
 
 export default function Collection() {
     const [catData, setCatData] = useState([
         {
-            catName :"Explore",
-            totalCatItem : "80 Resources",
-            catItemList : [
+            catName: "Explore",
+            totalCatItem: "80 Resources",
+            catItemList: [
                 {
-                    catImage:"./assets/poplar_image/img1.png",
-                    like:2,
+                    catImage: "./assets/poplar_image/img1.png",
+                    like: 2,
                 },
                 {
-                    catImage:"./assets/poplar_image/img1.png",
-                    like:2,
+                    catImage: "./assets/poplar_image/img1.png",
+                    like: 2,
                 },
                 {
-                    catImage:"./assets/poplar_image/img1.png",
-                    like:2,
+                    catImage: "./assets/poplar_image/img1.png",
+                    like: 2,
                 },
                 {
-                    catImage:"./assets/poplar_image/img1.png",
-                    like:2,
+                    catImage: "./assets/poplar_image/img1.png",
+                    like: 2,
                 },
                 {
-                    catImage:"./assets/poplar_image/img1.png",
-                    like:2,
+                    catImage: "./assets/poplar_image/img1.png",
+                    like: 2,
                 },
                 {
-                    catImage:"./assets/poplar_image/img1.png",
-                    like:2,
-                },
-            ]
-        },
-        {
-            catName :"Explore",
-            totalCatItem : "80 Resources",
-            catItemList : [
-                {
-                    catImage:"./assets/poplar_image/img1.png",
-                    like:2,
-                },
-                {
-                    catImage:"./assets/poplar_image/img1.png",
-                    like:2,
-                },
-                {
-                    catImage:"./assets/poplar_image/img1.png",
-                    like:2,
-                },
-                {
-                    catImage:"./assets/poplar_image/img1.png",
-                    like:2,
-                },
-                {
-                    catImage:"./assets/poplar_image/img1.png",
-                    like:2,
-                },
-                {
-                    catImage:"./assets/poplar_image/img1.png",
-                    like:2,
+                    catImage: "./assets/poplar_image/img1.png",
+                    like: 2,
                 },
             ]
         },
         {
-            catName :"Explore",
-            totalCatItem : "80 Resources",
-            catItemList : [
+            catName: "Explore",
+            totalCatItem: "80 Resources",
+            catItemList: [
                 {
-                    catImage:"./assets/poplar_image/img1.png",
-                    like:2,
+                    catImage: "./assets/poplar_image/img1.png",
+                    like: 2,
                 },
                 {
-                    catImage:"./assets/poplar_image/img1.png",
-                    like:2,
+                    catImage: "./assets/poplar_image/img1.png",
+                    like: 2,
                 },
                 {
-                    catImage:"./assets/poplar_image/img1.png",
-                    like:2,
+                    catImage: "./assets/poplar_image/img1.png",
+                    like: 2,
                 },
                 {
-                    catImage:"./assets/poplar_image/img1.png",
-                    like:2,
+                    catImage: "./assets/poplar_image/img1.png",
+                    like: 2,
                 },
                 {
-                    catImage:"./assets/poplar_image/img1.png",
-                    like:2,
+                    catImage: "./assets/poplar_image/img1.png",
+                    like: 2,
                 },
                 {
-                    catImage:"./assets/poplar_image/img1.png",
-                    like:2,
-                },
-            ]
-        },
-        {
-            catName :"Explore",
-            totalCatItem : "80 Resources",
-            catItemList : [
-                {
-                    catImage:"./assets/poplar_image/img1.png",
-                    like:2,
-                },
-                {
-                    catImage:"./assets/poplar_image/img1.png",
-                    like:2,
-                },
-                {
-                    catImage:"./assets/poplar_image/img1.png",
-                    like:2,
-                },
-                {
-                    catImage:"./assets/poplar_image/img1.png",
-                    like:2,
-                },
-                {
-                    catImage:"./assets/poplar_image/img1.png",
-                    like:2,
-                },
-                {
-                    catImage:"./assets/poplar_image/img1.png",
-                    like:2,
+                    catImage: "./assets/poplar_image/img1.png",
+                    like: 2,
                 },
             ]
         },
         {
-            catName :"Explore",
-            totalCatItem : "80 Resources",
-            catItemList : [
+            catName: "Explore",
+            totalCatItem: "80 Resources",
+            catItemList: [
                 {
-                    catImage:"./assets/poplar_image/img1.png",
-                    like:2,
+                    catImage: "./assets/poplar_image/img1.png",
+                    like: 2,
                 },
                 {
-                    catImage:"./assets/poplar_image/img1.png",
-                    like:2,
+                    catImage: "./assets/poplar_image/img1.png",
+                    like: 2,
                 },
                 {
-                    catImage:"./assets/poplar_image/img1.png",
-                    like:2,
+                    catImage: "./assets/poplar_image/img1.png",
+                    like: 2,
                 },
                 {
-                    catImage:"./assets/poplar_image/img1.png",
-                    like:2,
+                    catImage: "./assets/poplar_image/img1.png",
+                    like: 2,
                 },
                 {
-                    catImage:"./assets/poplar_image/img1.png",
-                    like:2,
+                    catImage: "./assets/poplar_image/img1.png",
+                    like: 2,
                 },
                 {
-                    catImage:"./assets/poplar_image/img1.png",
-                    like:2,
+                    catImage: "./assets/poplar_image/img1.png",
+                    like: 2,
+                },
+            ]
+        },
+        {
+            catName: "Explore",
+            totalCatItem: "80 Resources",
+            catItemList: [
+                {
+                    catImage: "./assets/poplar_image/img1.png",
+                    like: 2,
+                },
+                {
+                    catImage: "./assets/poplar_image/img1.png",
+                    like: 2,
+                },
+                {
+                    catImage: "./assets/poplar_image/img1.png",
+                    like: 2,
+                },
+                {
+                    catImage: "./assets/poplar_image/img1.png",
+                    like: 2,
+                },
+                {
+                    catImage: "./assets/poplar_image/img1.png",
+                    like: 2,
+                },
+                {
+                    catImage: "./assets/poplar_image/img1.png",
+                    like: 2,
+                },
+            ]
+        },
+        {
+            catName: "Explore",
+            totalCatItem: "80 Resources",
+            catItemList: [
+                {
+                    catImage: "./assets/poplar_image/img1.png",
+                    like: 2,
+                },
+                {
+                    catImage: "./assets/poplar_image/img1.png",
+                    like: 2,
+                },
+                {
+                    catImage: "./assets/poplar_image/img1.png",
+                    like: 2,
+                },
+                {
+                    catImage: "./assets/poplar_image/img1.png",
+                    like: 2,
+                },
+                {
+                    catImage: "./assets/poplar_image/img1.png",
+                    like: 2,
+                },
+                {
+                    catImage: "./assets/poplar_image/img1.png",
+                    like: 2,
                 },
             ]
         },
     ])
-   
 
-    function collectItem(imgItem,imgI){
-          
-                return(
-                    <>
-                   { imgI.map((item,index)=>{
-                       return(
-                        <>  
-                           
-                            
-                                
-                                {(index < 4)? 
+
+    function collectItem(imgItem, imgI) {
+
+        return (
+            <>
+                {imgI.map((item, index) => {
+                    return (
+                        <>
+
+
+
+                            {(index < 4) ?
                                 <>
-                                    {(imgItem === 1) ? 
+                                    {(imgItem === 1) ?
                                         <>
-                                            {(index < 2)? 
-                                                <li className={`collect_item `}  key={index}>
+                                            {(index < 2) ?
+                                                <li className={`collect_item `} key={index}>
                                                     <div className='collect_head'>
-                                                        <div className='display_h5'><img src={item?.catImage}/> </div>
-                                                    
+                                                        <div className='display_h5'><img src={item?.catImage} /> </div>
+
                                                     </div>
-                                                </li> 
-                                            :""}
+                                                </li>
+                                                : ""}
                                         </>
-                                    :
-                                    <li className={`collect_item `}  key={index}>
-                                        <div className='collect_head'>
-                                            <div className='display_h5'><img src={item?.catImage}/> </div>
-                                        
-                                        </div>
-                                    </li> 
+                                        :
+                                        <li className={`collect_item `} key={index}>
+                                            <div className='collect_head'>
+                                                <div className='display_h5'><img src={item?.catImage} /> </div>
+
+                                            </div>
+                                        </li>
                                     }
                                 </>
-                                :""}
-                            
+                                : ""}
+
                         </>
-                       )
-                    })}
-                </>
-                )
-        
+                    )
+                })}
+            </>
+        )
+
     }
-return (
-    <>
-        <div className='container-fluid tranding_sec'>
-            <div className='row'>
-                <div className='tranding_head col-md-12'>
-                    <div className='display_h3'>Collection</div>
+    return (
+        <>
+            <div className='container-fluid tranding_sec'>
+                <div className='row'>
+                    <div className='tranding_head col-md-12'>
+                        <div className='display_h3'>Collection</div>
+                    </div>
                 </div>
-            </div>
-            <div className='row'>
-                <div className='col-md-12'>
-                    <ul className='collect_img_grid'>
-                       
-                         {catData?.map((collect_item,index)=>{
-                            
-                                return(
-                                    <> 
-                                        {(index < 4) ? 
-                                        <li className='collect_item' key={index}>
-                                            <div className='collect_head'>
-                                                <div className='display_h5'>{collect_item.catName} </div>
-                                                <div className='coll_tot_item'>{collect_item?.totalCatItem}</div>
-                                                 <div className='imag_collect'> 
-                                                    <ul className={`${(index === 0) ? "first_img_block" : (index === 1) ? "two_eql_block" : (index === 2) ? "third_img_block" :  (index === 3) ? "fourth_eql_block" : "" }`}> 
-                                                        {collectItem(index,collect_item?.catItemList)} 
-                                                    </ul>
-                                                </div> 
-                                            </div>
-                                        
-                                        </li> 
-                                         : ""}
+                <div className='row'>
+                    <div className='col-md-12'>
+                        <ul className='collect_img_grid'>
+
+                            {catData?.map((collect_item, index) => {
+
+                                return (
+                                    <>
+                                        {(index < 4) ?
+                                            <li className='collect_item' key={index}>
+                                                <div className='collect_head'>
+                                                    <div className='display_h5'>{collect_item.catName} </div>
+                                                    <div className='coll_tot_item'>{collect_item?.totalCatItem}</div>
+                                                    <div className='imag_collect'>
+                                                        <ul className={`${(index === 0) ? "first_img_block" : (index === 1) ? "two_eql_block" : (index === 2) ? "third_img_block" : (index === 3) ? "fourth_eql_block" : ""}`}>
+                                                            {collectItem(index, collect_item?.catItemList)}
+                                                        </ul>
+                                                    </div>
+                                                </div>
+
+                                            </li>
+                                            : ""}
                                     </>
                                 )
-                               
-                            
-                        })} 
-                                                    
-                    </ul>
+
+
+                            })}
+
+                        </ul>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col-md-12 text-center pt-50'>
+                        <ViewMoreBtn
+                            btnContent="View more"
+                            btnLink="#"
+                        />
+                    </div>
                 </div>
             </div>
-            <div className='row'>
-                <div className='col-md-12 text-center pt-50'>
-                    <ViewMoreBtn 
-                        btnContent = "View more"
-                        btnLink="#"
-                    />
-                </div>
-            </div>             
-        </div>
-    </>
-  )
+        </>
+    )
 }
--- src/components/Popular/Popular.js
+++ src/components/Popular/Popular.js
@@ -1,81 +1,81 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import ViewMoreBtn from "../ViewMoreBtn/index";
 import { Link } from 'react-router-dom';
 import "./Popular.css";
 
 export default function Popular() {
-    const [trandingImgList,setTrandingImgList] = useState([{
-            trandingImg:"./assets/poplar_image/img1.png",
-            trandingCon:"dssd",
-            trandingCatName:"",
-            poplerImge:true,
-            like:2,
-            thumbUserIcon:"./assets/icons/user.png"
-        },
-        {
-            trandingImg:"./assets/poplar_image/img2.png",
-            trandingCon:"dssd",
-            trandingCatName:"",
-            poplerImge:true,
-            like:2,
-            thumbUserIcon:"./assets/icons/user.png"
-        },
-        {
-            trandingImg:"./assets/poplar_image/img3.png",
-            trandingCon:"dssd",
-            trandingCatName:"",
-            poplerImge:true,
-            like:2,
-            thumbUserIcon:"./assets/icons/user.png"
-        },
-        {
-            trandingImg:"./assets/poplar_image/img5.png",
-            trandingCon:"dssd",
-            trandingCatName:"",
-            poplerImge:true,
-            like:2,
-            thumbUserIcon:"./assets/icons/user.png"
-        },
-        {
-            trandingImg:"./assets/poplar_image/img4.png",
-            trandingCon:"dssd",
-            trandingCatName:"",
-            poplerImge:true,
-            like:0,
-            thumbUserIcon:"./assets/icons/user.png"
-        },
-        {
-            trandingImg:"./assets/poplar_image/img6.png",
-            trandingCon:"dssd",
-            trandingCatName:"",
-            poplerImge:true,
-            like:2,
-            thumbUserIcon:"./assets/icons/user.png"
-        },
-        {
-            trandingImg:"./assets/poplar_image/img7.png",
-            trandingCon:"dssd",
-            trandingCatName:"",
-            poplerImge:true,
-            like:0,
-            thumbUserIcon:"./assets/icons/user.png"
-        },
-        {
-            trandingImg:"./assets/poplar_image/img8.png",
-            trandingCon:"dssd",
-            trandingCatName:"",
-            poplerImge:true,
-            like:2,
-            thumbUserIcon:"./assets/icons/user.png"
-        },
-        {
-            trandingImg:"./assets/poplar_image/img9.png",
-            trandingCon:"dssd",
-            trandingCatName:"",
-            poplerImge:true,
-            like:0,
-            thumbUserIcon:"./assets/icons/user.png"
-        },
+    const [trandingImgList, setTrandingImgList] = useState([{
+        trandingImg: "./assets/poplar_image/img1.png",
+        trandingCon: "dssd",
+        trandingCatName: "",
+        poplerImge: true,
+        like: 2,
+        thumbUserIcon: "./assets/icons/user.png"
+    },
+    {
+        trandingImg: "./assets/poplar_image/img2.png",
+        trandingCon: "dssd",
+        trandingCatName: "",
+        poplerImge: true,
+        like: 2,
+        thumbUserIcon: "./assets/icons/user.png"
+    },
+    {
+        trandingImg: "./assets/poplar_image/img3.png",
+        trandingCon: "dssd",
+        trandingCatName: "",
+        poplerImge: true,
+        like: 2,
+        thumbUserIcon: "./assets/icons/user.png"
+    },
+    {
+        trandingImg: "./assets/poplar_image/img5.png",
+        trandingCon: "dssd",
+        trandingCatName: "",
+        poplerImge: true,
+        like: 2,
+        thumbUserIcon: "./assets/icons/user.png"
+    },
+    {
+        trandingImg: "./assets/poplar_image/img4.png",
+        trandingCon: "dssd",
+        trandingCatName: "",
+        poplerImge: true,
+        like: 0,
+        thumbUserIcon: "./assets/icons/user.png"
+    },
+    {
+        trandingImg: "./assets/poplar_image/img6.png",
+        trandingCon: "dssd",
+        trandingCatName: "",
+        poplerImge: true,
+        like: 2,
+        thumbUserIcon: "./assets/icons/user.png"
+    },
+    {
+        trandingImg: "./assets/poplar_image/img7.png",
+        trandingCon: "dssd",
+        trandingCatName: "",
+        poplerImge: true,
+        like: 0,
+        thumbUserIcon: "./assets/icons/user.png"
+    },
+    {
+        trandingImg: "./assets/poplar_image/img8.png",
+        trandingCon: "dssd",
+        trandingCatName: "",
+        poplerImge: true,
+        like: 2,
+        thumbUserIcon: "./assets/icons/user.png"
+    },
+    {
+        trandingImg: "./assets/poplar_image/img9.png",
+        trandingCon: "dssd",
+        trandingCatName: "",
+        poplerImge: true,
+        like: 0,
+        thumbUserIcon: "./assets/icons/user.png"
+    },
         // {
         //     trandingImg:"./assets/poplar_image/img10.png",
         //     trandingCon:"dssd",
@@ -85,13 +85,16 @@ export default function Popular() {
         //     thumbUserIcon:"./assets/poplar_image/user.png"
         // }
     ])
-    const [fliterCount,setFliterCount] = useState(0)
-    const [popValue,setPopValue]= useState(false)
-    const showPopImages = () =>{
+    const [fliterCount, setFliterCount] = useState(0)
+    const [popValue, setPopValue] = useState(false)
+    const showPopImages = () => {
         setPopValue(false)
     }
-    const showNewImages = () =>{
+    const showNewImages = () => {
         setPopValue(true)
+    }
+    const handleImage = () => {
+
     }
     return (
         <>
@@ -108,11 +111,11 @@ export default function Popular() {
                                 <li className='cat_menu_item'><a className='cat_menu_link' href="#">All</a></li>
                             </ul>
                             <div className='cat_fliter_sec'>
-                                <button><img className='img-fluid' src="./assets/icons/fliter.png"/>Filters ({fliterCount})</button>
+                                <button><img className='img-fluid' src="./assets/icons/fliter.png" />Filters ({fliterCount})</button>
                                 <div className=''></div>
                             </div>
                         </div>
-                        
+
                     </div>
                 </div>
             </div>
@@ -121,49 +124,49 @@ export default function Popular() {
                     <div className='tranding_head col-md-12'>
                         <div className='display_h3'>{popValue ? " New " : "Popular"}</div>
                         <div className='popular_btn_list'>
-                            <button className={`btn_link ${popValue ? " " : " active"} `} onClick={()=>showPopImages()}>Popular</button>
-                            <button  className={`btn_link ${popValue ? "active" : " "} `} onClick={()=>showNewImages()}>New</button>
+                            <button className={`btn_link ${popValue ? " " : " active"} `} onClick={() => showPopImages()}>Popular</button>
+                            <button className={`btn_link ${popValue ? "active" : " "} `} onClick={() => showNewImages()}>New</button>
                         </div>
                     </div>
                 </div>
                 <div className='row'>
-                     <div className='col-md-12'>
+                    <div className='col-md-12'>
                         <ul className='popular_img_grid'>
-                            {trandingImgList?.map((trand_content,index)=>{
-                                return(
+                            {trandingImgList?.map((trand_content, index) => {
+                                return (
                                     <li className='popular_img_grid_item' key={index}>
                                         <div className='popular_img'>
-                                            <img src={trand_content.trandingImg} className='img-fluid'/>
-                                           
+                                            <img src={trand_content.trandingImg} className='img-fluid' onClick={() => handleImage()} />
+
                                             <div className='top_img_con'>
-                                                <button className='img_hover_btn'><div className='no_like'>{trand_content.like }</div><img src={trand_content.like ? "./assets/icons/like.png" : "./assets/icons/liked.png"} className='img-fluid'/></button>
-                                                <button className='img_hover_btn'><img src="./assets/icons/add.png" className='img-fluid'/></button>
+                                                <button className='img_hover_btn'><div className='no_like'>{trand_content.like}</div><img src={trand_content.like ? "./assets/icons/like.png" : "./assets/icons/liked.png"} className='img-fluid' /></button>
+                                                <button className='img_hover_btn'><img src="./assets/icons/add.png" className='img-fluid' /></button>
                                             </div>
                                             <div className='bottom_img_con'>
                                                 <div className='upload_user_cont'>
                                                     <div className='upload_user'>
-                                                        <img src={trand_content.thumbUserIcon ? trand_content.thumbUserIcon  :"./assets/poplar_image/user.png"} className='img-fluid'/>
+                                                        <img src={trand_content.thumbUserIcon ? trand_content.thumbUserIcon : "./assets/poplar_image/user.png"} className='img-fluid' />
                                                     </div>
                                                     <div className='upload_disc'>
                                                         {trand_content.trandingCon}
                                                     </div>
                                                 </div>
-                                                <button className='img_hover_btn'><img src="./assets/icons/download.png" className='img-fluid'/></button>
+                                                <button className='img_hover_btn'><img src="./assets/icons/download.png" className='img-fluid' /></button>
                                             </div>
-                                            
+
                                         </div>
-                                    </li> 
-                                ) 
+                                    </li>
+                                )
                             })}
-                                                    
+
                         </ul>
                     </div>
                 </div>
 
                 <div className='row'>
                     <div className='col-md-12 text-center pt-50'>
-                        <ViewMoreBtn 
-                            btnContent = "View more"
+                        <ViewMoreBtn
+                            btnContent="View more"
                             btnLink="#"
                         />
                     </div>
