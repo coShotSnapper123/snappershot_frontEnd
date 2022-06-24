8f8132681766c06b034202baa4035bf79982c815
shubham <shubham@shubhams-MacBook-Air.local>
2022-06-24 14:39:11 +0530

    code update

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.render(
       <App />
     </Suspense> 
   </React.StrictMode>,
-  document.getElementById('message')
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
